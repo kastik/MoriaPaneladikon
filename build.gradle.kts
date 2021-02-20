47fde096e929565541366d5c113234ac7f6577e2
kastik <kostas21422112@gmail.com>
2021-02-20 19:15:48 +0200

    --Update Dependencies--
    Signed-off-by: kastik <kostas21422112@gmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    //ext.kotlin_version = '1.4.21-2'
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath("com.google.gms:google-services:4.3.5")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.4.1")
+        classpath("com.google.firebase:perf-plugin:1.3.4")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
